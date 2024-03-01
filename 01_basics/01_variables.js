5fbe81a63aca8adb89342b73ffc949eabcccda64
harishkhedekar <67747613+harishkhedekar@users.noreply.github.com>
2024-03-01 06:01:46 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "harishk123@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+let accountState;
+
+// accountId = 2 //not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountCity = "Pune"
+
+console.log(accountId);
+
+/*
+prefer not to use var because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail,accountPassword,accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3) // code readability should be high
+
+console.log("Hello")
+
+console.log(typeof "hello");
+
+console.log(typeof null); //object
+
+console.log(typeof undefined); //undefined
