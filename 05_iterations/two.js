ffda31ce2b44194c4fd0c1312fb1dda90cc333e6
harishkhedekar <67747613+harishkhedekar@users.noreply.github.com>
2024-03-05 04:40:00 +0000

    while and do while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+let i = 0
+
+// while (i <= 10) {
+//     console.log(`Value of i is ${i}`);
+//     i++
+// }
+
+let A = ["Flash", "Batman", "Superman"]
+let arr = 0
+// while (arr < A.length) {
+//     console.log(`Value is ${A[arr]}`);
+//     arr = arr + 1
+// }
+
+
+let score = 1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
